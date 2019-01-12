4afa33b930782e65e3f3f6c736049592f6eec4ae
jkelley399 <jkelley@399innovation.com>
2019-01-11 19:15:24 -0800

    fix: Changes var to let & double to single quotes
    Per UDAC FNSG:
    1.  Changes var to let for variable declarations
    2.  Changes double quotes to single quotes

--- designs.js
+++ designs.js
@@ -3,7 +3,12 @@
 
 // When size is submitted by the user, call makeGrid()
 
-/*2019-01-07: Added additional functionality
+/*
+2019-01-11: Reviewed style guides
+    Changes var to let for variable declarations (all seems to work ok)
+    Could not find a UDAC-approved JavaScript validator, so not doing that
+
+2019-01-07: Added additional functionality
     double-click on cell returns it to white
     double-click on targeSubmitButton removes existing grid
 
@@ -13,22 +18,22 @@
     2.  Run through validators and complete assignment.
 */
 
-var colorInput = document.querySelector('#colorPicker');
-var targetSubmitButton = document.querySelector("#sizePickerSubmitButton");
-var targetDiv = document.querySelector("#pixelCanvas");
-var inputForm = document.querySelector('#sizePicker');
-var chosenColor = "#000000";
+let colorInput = document.querySelector('#colorPicker');
+let targetSubmitButton = document.querySelector('#sizePickerSubmitButton');
+let targetDiv = document.querySelector('#pixelCanvas');
+let inputForm = document.querySelector('#sizePicker');
+let chosenColor = '#000000';
 
 function makeGrid() {
     let inColor = inputForm.value;
     let inHt = inputForm.inputHeight.value;
     let inWh = inputForm.inputWidth.value;
     for(let i = 0; i < inHt; i++){
-        console.log ("inHt = " + inHt);
+        console.log ('inHt = ' + inHt);
         let newRow = document.createElement('tr');
         targetDiv.appendChild(newRow);
             for(let j = 0; j < inWh; j++){
-                console.log ("inWh = " + inWh);
+                console.log ('inWh = ' + inWh);
                 let newCell = document.createElement('td');
                 newRow.appendChild(newCell);
                 }
@@ -40,12 +45,12 @@ function changeColor(evt) {
     evt.target.style.backgroundColor = chosenColor;
 }
 
-colorPicker.addEventListener("input", function () {
+colorPicker.addEventListener('input', function () {
     chosenColor = event.target.value;
 });
 
 function removeColor(evt) {
-    evt.target.style.backgroundColor = "#ffffff";
+    evt.target.style.backgroundColor = '#ffffff';
 }
 
 function removeGrid() {
